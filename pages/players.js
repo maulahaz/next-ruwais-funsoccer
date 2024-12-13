da134dd57d78655539a6d015b13a7aa7fb7bf8a7
maulahaz <maulahaz@gmail.com>
2024-12-13 22:10:39 +0400

    Add some photo players, and Fix filter by Team name

--- pages/players.js
+++ pages/players.js
@@ -9,21 +9,25 @@ export default function Players() {
   const [totalPages, setTotalPages] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
   const playersPerPage = 12;
+  const [teams, setTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState(null);
 
-  useEffect(() => {
-    fetchPlayers();
-  }, [currentPage, searchTerm]);
+  const fetchTeams = async () => {
+    const { data, error } = await supabase
+      .from("teams")
+      .select("id, name, team_alias")
+      .order("name");
+    if (error) {
+      console.error("Error fetching teams:", error);
+    } else {
+      setTeams(data);
+    }
+  };
 
   const fetchPlayers = async () => {
     let query = supabase
       .from("players")
-      .select(
-        `
-        *,
-        team:team_id (name, team_alias)
-      `,
-        { count: "exact" }
-      )
+      .select(`*, team:team_id (name, team_alias)`, { count: "exact" })
       .order("name", { ascending: true })
       .range(
         (currentPage - 1) * playersPerPage,
@@ -31,10 +35,17 @@ export default function Players() {
       );
 
     if (searchTerm) {
-      query = query.ilike("name", `%${searchTerm}%`);
+      query = query.or(
+        `name.ilike.%${searchTerm}%, alias.ilike.%${searchTerm}%`
+      );
     }
-
+    //
+    if (selectedTeam) {
+      query = query.eq("team_id", selectedTeam);
+    }
+    //
     const { data, error, count } = await query;
+    // console.log("Result:", { data, error, count });
 
     if (error) {
       console.error("Error fetching players:", error);
@@ -51,12 +62,23 @@ export default function Players() {
 
   const handleReset = () => {
     setSearchTerm("");
+    setSelectedTeam(null);
     setCurrentPage(1);
   };
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
+
+  const handleTeamSelect = (teamId) => {
+    setSelectedTeam(teamId === selectedTeam ? null : teamId);
+    setCurrentPage(1);
+  };
+
+  useEffect(() => {
+    fetchTeams();
+    fetchPlayers();
+  }, [currentPage, searchTerm, selectedTeam]);
 
   return (
     <div className="bg-black min-h-screen text-white">
@@ -67,20 +89,42 @@ export default function Players() {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold mb-8 text-center">Players</h1>
 
-        <div className="flex mb-4">
-          <input
-            type="text"
-            placeholder="Search player..."
-            value={searchTerm}
-            onChange={handleSearch}
-            className="flex-grow p-2 border rounded-l text-black"
-          />
-          <button
-            onClick={handleReset}
-            className="bg-gray-500 text-white px-4 py-2 rounded-r hover:bg-blue-700 transition-colors"
-          >
-            Reset
-          </button>
+        <div className="flex flex-col mb-4">
+          <h3 className="text-lg mb-2 mr-4">Filter by name or alias:</h3>
+          <div className="flex">
+            <input
+              id="searchInput"
+              type="text"
+              placeholder="Enter player name or alias..."
+              value={searchTerm}
+              onChange={handleSearch}
+              className="flex-grow p-2 border rounded-l text-black"
+            />
+            <button
+              onClick={handleReset}
+              className="bg-gray-500 text-white px-4 py-2 rounded-r hover:bg-blue-700 transition-colors"
+            >
+              Reset All
+            </button>
+          </div>
+        </div>
+
+        {/* Filter by Team */}
+        <div className="mb-4 flex flex-wrap gap-2">
+          <h3 className="text-lg mb-2 mr-4">Filter by Team:</h3>
+          {teams.map((team) => (
+            <button
+              key={team.id}
+              onClick={() => handleTeamSelect(team.id)}
+              className={`px-3 py-1 rounded ${
+                selectedTeam === team.id
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 text-black"
+              } hover:bg-blue-600 hover:text-white transition-colors`}
+            >
+              {team.team_alias}
+            </button>
+          ))}
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -91,24 +135,22 @@ export default function Players() {
             >
               <div className="mr-4 flex-shrink-0">
                 <a href={player.id}>
-                <div className="w-24 h-24 rounded-full border-2 border-white overflow-hidden">
-                  <img
-                    src={
-                      player.photo
-                        ? `/players/${player.id}.jpg`
-                        : `/players/user.jpg`
-                    }
-                    alt={player.name}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+                  <div className="w-24 h-24 rounded-full border-2 border-white overflow-hidden">
+                    <img
+                      src={
+                        player.photo
+                          ? `/players/${player.id}.jpg`
+                          : `/players/user.jpg`
+                      }
+                      alt={player.name}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
                 </a>
                 <h2 className="text-xl font-semibold mt-2 text-center">
                   {player.name}
                 </h2>
-                <p className="text-sm text-center">
-                  {player.alias ?? "-"}
-                </p>
+                <p className="text-sm text-center">{player.alias ?? "-"}</p>
               </div>
               <div className="flex-grow">
                 <p>
